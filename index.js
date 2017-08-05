b21cb4dbeee7ebd70e089756107b6abd0ed6d5bb
shikhi7 <mshikhar29@gmail.com>
2017-08-05 07:13:13 +0530

    Added socket module & its primary use in server

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('Yo! A user got connected, or maybe not ;)');
+  socket.on('disconnect', function(){
+  	console.log('No! A user got disconnected, or maybe not ;)');
+  })
 });
 
 http.listen(3000, function(){
