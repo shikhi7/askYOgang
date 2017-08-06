1a9c15090b60be1abd4ec13b51fccc1f914f80b0
shikhi7 <mshikhar29@gmail.com>
2017-08-06 17:12:43 +0530

    wip for serving files in public dir

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
+//var express=require('express');
+//var app=express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+//var path=require('path');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+//app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
   console.log('Yo! A user got connected, or maybe not ;)');
