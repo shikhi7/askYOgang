51da2f4cab2c56da9a1bea502b5568c2841c42bf
RaiManish3 <manish.07.1996@gmail.com>
2018-03-22 01:51:12 +0530

    css file in path

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var app = require('express')();
-express=require('express');
-//var app=express();
+var express=require('express');
+var app=express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//var path=require('path');
+var path=require('path');
 
+app.use(express.static(path.join(__dirname, '/public')))
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
