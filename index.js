5aaa1cfbe5e8e430f5a3384b76f9148f89e9e828
RaiManish3 <manish.07.1996@gmail.com>
2018-03-23 20:14:41 +0530

    login form; sql database basic

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ var app=express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path=require('path');
+var mysql = require('mysql');
 
 app.use(express.static(path.join(__dirname, '/public')))
-app.get('/', function(req, res){
+
+app.get('/home', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/login.html');
+});
 
-app.use(express.static(__dirname+'/public'));
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "username",
+  password: "password",
+  database: "database-name"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to the database!");
+})
 
 io.on('connection', function(socket){
   console.log('Yo! A user got connected, or maybe not ;)');
@@ -22,7 +37,33 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-});
+
+  socket.on('login_credentials', function(username, pass){
+      let sql_query = "SELECT * FROM askYogang WHERE username='" +
+                      username+"' AND pass='"+pass + "';";
+      con.query(sql_query, function (err, result) {
+          if (err) throw err;
+          if(result.length > 0)
+            // TODO :: fetch the next page
+            io.emit('accessAllowed', 'you are allowed, baby');
+          else io.emit('accessDenied', 'Stay Away!!');
+      });
+    });
+
+
+  socket.on('register_credentials', function(username, pass){
+      // TODO :: Check whether the username is distinct
+      let sql_query = "INSERT INTO askYogang (username, pass) VALUES ('" +
+                        username+"','" + pass + "')";
+      con.query(sql_query, function (err, result) {
+          if (err) throw err;
+          // TODO :: fetch the next page
+          io.emit('accessAllowed', 'you are allowed, baby');
+      });
+    });
+
+})
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
